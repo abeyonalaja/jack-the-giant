f7de13ee4205b636d92c5a2f53193b89a3237b86
Abey Onalaja <abey.onalaja@gmail.com>
2016-05-11 02:17:01 +0100

    Create BackgroundScaler script to auto resize background width.

---
+++ Assets/Scripts/BackgroundScaler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScaler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		SpriteRenderer spriteRender = GetComponent<SpriteRenderer>();
+		Vector3 tempScale = transform.localScale;
+		
+		float width = spriteRender.sprite.bounds.size.x;
+		
+		float worldHeight = Camera.main.orthographicSize * 2.0f;
+		float worldWidth = worldHeight / Screen.height * Screen.width;
+		
+		tempScale.x = worldWidth / width;
+		
+		transform.localScale = tempScale;
+	}
+	
+}
