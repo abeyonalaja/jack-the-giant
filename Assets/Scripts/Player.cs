726d076e382a094b3610dfb9da15995940dd467c
Abey Onalaja <abey.onalaja@gmail.com>
2016-05-11 01:09:05 +0100

    Add player controller script to player prefab

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 8f;
+	public float maxVelocity = 4;
+	
+	private Rigidbody2D playerBody;
+	private Animator playerAnimatior;
+	
+	void Awake() {
+		playerBody = GetComponent<Rigidbody2D>();
+		playerAnimatior = GetComponent<Animator>();
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void FixedUpdate() {
+		PlayerMoveKeyboard();
+	}
+	
+	void PlayerMoveKeyboard() {
+		float forceX = 0f;
+		float vel = Mathf.Abs(playerBody.velocity.x);
+		
+		float h = Input.GetAxisRaw("Horizontal");
+		
+		if(h > 0) {
+			if(vel < maxVelocity)
+				forceX = speed;
+		} else if(h < 0) {
+			if(vel < maxVelocity)
+				forceX = -speed;
+		} else {
+			
+		}
+		
+		playerBody.AddForce(new Vector2(forceX, 0) );
+	}
+}
