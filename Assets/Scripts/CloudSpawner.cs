59dba8597dfe1aa2c237fd9457381fc26fac36fe
Abey Onalaja <abey.onalaja@gmail.com>
2016-05-16 20:01:50 +0100

    Spawn clouds in random positions

--- Assets/Scripts/CloudSpawner.cs
+++ Assets/Scripts/CloudSpawner.cs
@@ -20,12 +20,55 @@ public class CloudSpawner : MonoBehaviour {
 	
 	private GameObject player;
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+		controlX = 0;
+		SetMinAndMaxX();
+		CreageClouds();
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void SetMinAndMaxX(){
+		Vector3 bounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+		
+		maxX = bounds.x - 0.5f;
+		minX = -bounds.x + 0.5f;
+	}
 	
+	void Shuffle(GameObject[] arrayToShuffle){
+		for(int i = 0; i < arrayToShuffle.Length; i++) {
+			GameObject temp = arrayToShuffle[i];
+			int random = Random.Range(i, arrayToShuffle.Length);
+			arrayToShuffle[i] = arrayToShuffle[random];
+			arrayToShuffle[random] = temp;
+		}
+	}
+	
+	void CreageClouds() {
+		Shuffle(clouds);
+		
+		float positionY = 0f;
+		
+		for(int i = 0; i< clouds.Length; i++){
+			Vector3 temp = clouds[i].transform.position;
+			temp.y = positionY;
+			temp.x = Random.Range(minX, maxX);
+			
+			if(controlX == 0){
+				temp.x = Random.Range(0.0f, maxX);
+				controlX = 1;
+			} else if(controlX == 1){
+				temp.x = Random.Range(0.0f, minX);
+				controlX = 2;
+			} else if(controlX == 2) {
+				temp.x = Random.Range(1.0f, maxX);
+				controlX = 3;
+			} else if(controlX == 3) {
+				temp.x = Random.Range(-1.0f, minX);
+				controlX = 0;
+			}
+			
+			lastCloudPositionY = positionY;
+			clouds[i].transform.position = temp;
+			positionY -= distanceBetweenClouds;
+		}
 	}
 }
