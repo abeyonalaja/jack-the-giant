affb90466a5784cb8dbe17fa0e87056a5ea70f35
Abey Onalaja <abey.onalaja@gmail.com>
2016-05-19 23:51:54 +0100

    Add scene controllers

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	public void OnStartButtonClick() {
+		// Application.LoadLevel("main");
+		SceneManager.LoadScene("main");
+	}
+	
+	public void OnHighScoreButtonClick() {
+		SceneManager.LoadScene("HighScoreScene");
+	}
+	
+	public void OnOptionsButtonClick() {
+		SceneManager.LoadScene("OptionsMenuScene");
+	}
+	
+	public void OnQuitButtonClick() {
+		Application.Quit();
+	}
+}
