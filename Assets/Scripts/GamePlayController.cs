2e98cbe5df119d93f86b1bf3f925607be3616327
Abey Onalaja <abey.onalaja@gmail.com>
2016-05-21 18:23:59 +0100

    Impliment Singleton pattern with GamePlayController

---
+++ Assets/Scripts/GamePlayController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GamePlayController : MonoBehaviour {
+	
+	public static GamePlayController instance;
+
+	void GetInstance() {
+		if(instance == null) {
+			instance = this;
+		}
+	}
+	
+	void Awake() {
+		GetInstance();
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
